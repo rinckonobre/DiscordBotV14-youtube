b5587ee58e92cfe0a1a6384a0d0fa781d90bb75d
ricknobre <guilhermericardopro71@gmail.com>
2023-03-17 04:43:05 -0400

    Commands, Events and Components Handler

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,13 @@
 import { ExtendedClient } from "./structs/ExtendedClient"
 export * from "colors";
+import config from "./config.json";
+import path from "path";
+
+import fs from "fs";
 
 const client = new ExtendedClient();
-
 client.start();
 
-export { client }
+export { client, config }
 
-client.on("ready", () => {
-    console.log("Bot online".green);
-})
 
-client.on("messageCreate", (message) => {
-    if (message.author.id == client.user?.id) return;
-
-    message.reply({
-        content: `Olá ${message.author.username}`
-    })
-})
